3c73f419cfdc3b773c22d92221e4f5c9c5fdbd97
Peter Farr <Peter@PrismaPhonic.com>
2018-11-02 11:55:07 -0700

    added scoring logic

--- src/Scoring.js
+++ src/Scoring.js
@@ -5,14 +5,30 @@ import { ones, twos, threes, fours, fives, sixes, threeOfKind, fourOfKind, fullH
 
 
 class Scoring extends Component {
+  sumUpper() {
+    let upper = [this.props.scores.ones, this.props.scores.twos, this.props.scores.threes, this.props.scores.fours, this.props.scores.fives, this.props.scores.sixes]
+    return upper.map(score => this.convertIfUndefined(score)).reduce((a, b) => a + b, 0);
+  }
+
+  sumLower() {
+    let lower = [this.props.scores.threeOfKind, this.props.scores.fourOfKind, this.props.scores.fullHouse, this.props.scores.smallStraight, this.props.scores.largeStraight, this.props.scores.yahtzee, this.props.scores.chance]
+    return lower.map(score => this.convertIfUndefined(score)).reduce((a, b) => a + b, 0);
+  }
+
+  convertIfUndefined(score) {
+    return (score === undefined) ? 0 : score;
+  }
 
   render() {
+
     const { scores, doScore } = this.props;
 
     return (
       <div className="Scoring">
+        <h1>TOTAL SCORE: {this.sumLower() + this.sumUpper()}</h1>
         <section className="Scoring-section">
           <h2>Upper</h2>
+          <h5>Score: {this.sumUpper()}</h5>
           <table cellSpacing="0">
             <tbody>
               <Rule name="Ones" score={scores.ones} doScore={evt => doScore("ones", ones.evalRoll)} />
@@ -26,6 +42,7 @@ class Scoring extends Component {
         </section>
         <section className="Scoring-section Scoring-section-lower">
           <h2>Lower</h2>
+          <h5>Score: {this.sumLower()}</h5>
           <table cellSpacing="0">
             <tbody>
               <Rule name="Three of Kind" score={scores.threeOfKind} doScore={evt => doScore("threeOfKind", threeOfKind.evalRoll)} />
